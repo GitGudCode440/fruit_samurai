2196506c6d098b89d7e1fdc02ce6d36cbb481cd1
M. Mahad Hashmi <m.mahadhashmi@gmail.com>
2024-11-11 23:31:52 +0500

    Add a global.h file.

---
+++ globals.h
@@ -0,0 +1,11 @@
+#pragma once
+#define WINDOW_WIDTH 800
+#define WINDOW_HEIGHT 600
+
+#define ENTITY_AMOUNT 6
+#define FONT_SIZE 24
+#define INITIAL_UPWARD_VELOCITY (-360)
+#define GRAVITY 150
+#define WINDOW_BORDER_PADDING 16
+
+#define FRUIT_TEXTURE_PATH "res/textures/fruits"
